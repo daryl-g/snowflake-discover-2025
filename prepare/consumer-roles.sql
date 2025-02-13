fbba5b3ff36cd3864f28d0f994edc79994b3925b
Daryl <63649138+daryl-g@users.noreply.github.com>
2025-02-13 04:13:09 +0000

    Add code to grant permission for secondary role

--- prepare/consumer-roles.sql
+++ prepare/consumer-roles.sql
@@ -11,3 +11,11 @@ grant role chairlift_viewer to role chairlift_admin;
 -- ensure a warehouse is usable by consumer
 grant usage on warehouse chairlift_wh to role chairlift_admin;
 grant usage on warehouse chairlift_wh to role chairlift_viewer;
+
+-- allow restricted application access to a secondary role
+use role chairlift_admin;
+use warehouse chairlift_wh;
+
+-- allow our secondary viewer role restricted access to the app
+grant application role chairlift_app.app_viewer
+    to role chairlift_viewer;
